eacfd287aaa1a158c6521a17363cf51ee7a72eed
chiclaim <chiclaim@gmail.com>
2016-02-29 10:23:01 +0800

    construction reference

---
+++ src/com/java8/lambda/ConstructionReference.java
@@ -0,0 +1,14 @@
+package com.java8.lambda;
+/**
+ * 构造器引用
+ * @author yuzhiqiang
+ *
+ */
+public class ConstructionReference {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

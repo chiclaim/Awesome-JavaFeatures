5f01e011aaa2c76a18ceb8ec2c4e140b437c0a14
chiclaim <chiclaim@gmail.com>
2016-03-01 21:46:01 +0800

    构造器引用

---
+++ src/com/java8/lambda/A.java
@@ -0,0 +1,4 @@
+package com.java8.lambda;
+public  interface A {
+		public String a(String value);
+}
--- src/com/java8/lambda/ConstructionReference.java
+++ src/com/java8/lambda/ConstructionReference.java
@@ -1,14 +1,42 @@
 package com.java8.lambda;
+
 /**
  * 构造器引用
+ * 
  * @author yuzhiqiang
  *
  */
 public class ConstructionReference {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ConstructionReference reference = new ConstructionReference();
+		reference.test();
 	}
 
+	public void test() {
+		PersonFactory<Person> factory = Person::new;
+		Person person = factory.createPerson("san", "zhang");
+		System.out.println(person);
+	}
+
+	public interface PersonFactory<P extends Person> {
+		Person createPerson(String firstName, String lastName);
+	}
+
+	public class Person {
+
+		public String firstName;
+		public String lastName;
+
+		public Person(String firstName, String lastName) {
+			this.firstName = firstName;
+			this.lastName = lastName;
+		}
+
+		@Override
+		public String toString() {
+			return "Person [firstName=" + firstName + ", lastName=" + lastName + "]";
+		}
+		
+	}
 }

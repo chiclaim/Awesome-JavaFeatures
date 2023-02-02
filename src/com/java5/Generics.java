a0d39d007c8ccdd02bb4b4d858451fa0ee7ca248
chiclaim <chiclaim@gmail.com>
2023-02-02 11:20:54 +0800

    泛型说明

--- src/com/java5/Generics.java
+++ src/com/java5/Generics.java
@@ -3,7 +3,9 @@ package com.java5;
 /**
  * 泛型（不变性 (invariance)、协变性 (covariance)、逆变性 (contravariance)）
  *
- * 泛型的知识点比较多，可以参考我的：https://blog.csdn.net/yuzhiqiang666/article/details/85575213
+ * 泛型的知识点比较多，可以参考
+ * - 官网：https://www.oracle.com/technical-resources/articles/java/java-5-features3.html
+ * - 我的：https://blog.csdn.net/yuzhiqiang666/article/details/85575213
  */
 public class Generics {
 

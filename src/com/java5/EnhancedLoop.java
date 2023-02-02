4bee1458a450b49fedf11c4b2c26b8187a17e984
chiclaim <chiclaim@gmail.com>
2023-02-02 10:21:54 +0800

    for 循环增强注意事项

--- src/com/java5/EnhancedLoop.java
+++ src/com/java5/EnhancedLoop.java
@@ -15,5 +15,9 @@ public class EnhancedLoop {
         for (int number : numbers) {
             System.out.println(number);
         }
+
+        // 注意：
+        // for 循环增强，本质上是通过迭代器 iterator 实现的
+        // 如果在循环中删除，则需要使用传统的迭代器方式
     }
 }
